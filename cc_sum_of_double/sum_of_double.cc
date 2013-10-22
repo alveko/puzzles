d1b2245051b4fe826fb8235c7b10bf8807a905e1
Alexander Korobeynikov <alexander.korobeynikov@gmail.com>
2013-10-22 23:37:55 +0200

    2 queue summation algorithm

--- cc_sum_of_double/sum_of_double.cc
+++ cc_sum_of_double/sum_of_double.cc
@@ -1,4 +1,5 @@
 #include <vector>
+#include <queue>
 #include <algorithm> // std::min()
 #include <iostream>  // std::cin, std::cout
 #include <stdlib.h>  // rand()
@@ -56,8 +57,6 @@ void generate_double_vector(std::vector<double>& v, size_t n)
 {
     TRACE(("\nGenerating random vector...\n"));
     boost::timer::auto_cpu_timer t("Done in %t sec CPU, %w sec real\n");
-
-    srand(time(NULL));
     v.resize(n);
     std::generate(v.begin(), v.end(), generate_double);
 }
@@ -123,7 +122,7 @@ double sum3(const std::vector<double>& v)
 
 // Sums up all double values of the given array
 // O(N lonN) but faster than BST
-// Create a heap, sum two min value, insert the sum into the heap, repeat
+// Create a heap, sum two min values, insert the sum into the heap, repeat
 double sumH(std::vector<double>& v)
 {
     double d1, d2;
@@ -147,6 +146,48 @@ double sumH(std::vector<double>& v)
         std::push_heap(v.begin(), v.end(), std::greater<double>());
     }
     return !v.empty() ? *v.begin() : 0.0;
+}
+
+// Sums up all double values of the given array
+// O(N)
+// 2 queues algorithm
+double sumQ(std::vector<double>& v)
+{
+    std::queue<double> q_orig, q_sums;
+    if (v.size() == 0) {
+        return 0;
+    }
+
+    std::sort(v.begin(), v.end());
+    for (auto x : v) {
+        q_orig.push(x);
+    }
+
+    auto get_min = [&]() {
+        double d = 0;
+        if (!q_orig.empty() || !q_sums.empty()) {
+            if (q_orig.empty()) {
+                d = q_sums.front();
+                q_sums.pop();
+            } else if (q_sums.empty()) {
+                d = q_orig.front();
+                q_orig.pop();
+            } else if (q_orig.front() < q_sums.front()) {
+                d = q_orig.front();
+                q_orig.pop();
+            } else {
+                d = q_sums.front();
+                q_sums.pop();
+            }
+        }
+        return d;
+    };
+
+    while (q_orig.size() > 0 || q_sums.size() > 1) {
+        q_sums.push(get_min() + get_min());
+    }
+
+    return q_sums.front();
 }
 
 // Sums up all double values of the given array
@@ -187,6 +228,14 @@ double sumT(std::vector<double>& v)
     return s;
 }
 
+const uint8_t SUM_1 = (1<<0);
+const uint8_t SUM_2 = (1<<1);
+const uint8_t SUM_3 = (1<<2);
+const uint8_t SUM_H = (1<<3);
+const uint8_t SUM_T = (1<<4);
+const uint8_t SUM_Q = (1<<5);
+const uint8_t SUM_ALL = 0xFF;
+
 int main(int argc, char *argv[])
 {
     size_t input_size = 10;
@@ -194,8 +243,8 @@ int main(int argc, char *argv[])
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help,h", "Show help")
-        ("algo", po::value<std::string>()->default_value("sum3HT"),
-         "Sum algorithm:\n<sum1 | sum2 | sum3 | sum3HT | all>")
+        ("algo", po::value<std::string>()->default_value("sum3HQT"),
+         "Sum algorithm:\n<sum1 | sum2 | sum3 | sum3HQT | all>")
         ("size", po::value<size_t>(&input_size)->default_value(input_size),
          "Size of the array");
 
@@ -211,9 +260,29 @@ int main(int argc, char *argv[])
     TRACE(("Algo: %s\n")
           % vm["algo"].as<std::string>());
 
+    uint8_t sum = 0;
+    if (vm["algo"].as<std::string>() == "sum1") {
+        sum = SUM_1;
+    } else if (vm["algo"].as<std::string>() == "sum2") {
+        sum = SUM_2;
+    } else if (vm["algo"].as<std::string>() == "sum3") {
+        sum = SUM_3;
+    } else if (vm["algo"].as<std::string>() == "sumH") {
+        sum = SUM_H;
+    } else if (vm["algo"].as<std::string>() == "sumQ") {
+        sum = SUM_Q;
+    } else if (vm["algo"].as<std::string>() == "sumT") {
+        sum = SUM_T;
+    } else if (vm["algo"].as<std::string>() == "sum3HQT") {
+        sum = SUM_3 | SUM_H | SUM_Q | SUM_T;
+    } else if (vm["algo"].as<std::string>() == "all") {
+        sum = SUM_ALL;
+    }
+
+    srand(time(NULL));
     std::vector<double> input_vector, v;
     generate_double_vector(input_vector, input_size);
-    double s0 = 0.0, s1 = 0.0, s2 = 0.0, s3 = 0.0, sT = 0.0, sH = 0.0;
+    double s0 = 0.0, s1 = 0.0, s2 = 0.0, s3 = 0.0, sT = 0.0, sH = 0.0, sQ = 0.0;
 
     double vmax = 0.0, vmin = 1.0;
     for (auto d : input_vector) {
@@ -235,8 +304,7 @@ int main(int argc, char *argv[])
               % d2u(s0) % s0);
     }
 
-    if (vm["algo"].as<std::string>() == "sum1" ||
-        vm["algo"].as<std::string>() == "all") {
+    if (sum & SUM_1) {
         v = input_vector;
         TRACE(("\nCalculating sum1...\n"));
         boost::timer::auto_cpu_timer t("Done in %t sec CPU, %w sec real\n");
@@ -247,8 +315,7 @@ int main(int argc, char *argv[])
               % d2u(s1 - s0) % (s1 - s0));
     }
 
-    if (vm["algo"].as<std::string>() == "sum2" ||
-        vm["algo"].as<std::string>() == "all") {
+    if (sum & SUM_2) {
         v = input_vector;
         TRACE(("\nCalculating sum2...\n"));
         boost::timer::auto_cpu_timer t("Done in %t sec CPU, %w sec real\n");
@@ -259,9 +326,7 @@ int main(int argc, char *argv[])
               % d2u(s2 - s0) % (s2 - s0));
     }
 
-    if (vm["algo"].as<std::string>() == "sum3" ||
-        vm["algo"].as<std::string>() == "sum3HT" ||
-        vm["algo"].as<std::string>() == "all") {
+    if (sum & SUM_3) {
         v = input_vector;
         TRACE(("\nCalculating sum3...\n"));
         boost::timer::auto_cpu_timer t("Done in %t sec CPU, %w sec real\n");
@@ -272,9 +337,7 @@ int main(int argc, char *argv[])
               % d2u(s3 - s0) % (s3 - s0));
     }
 
-    if (vm["algo"].as<std::string>() == "sumH" ||
-        vm["algo"].as<std::string>() == "sum3HT" ||
-        vm["algo"].as<std::string>() == "all") {
+    if (sum & SUM_H) {
         v = input_vector;
         TRACE(("\nCalculating sumH...\n"));
         boost::timer::auto_cpu_timer t("Done in %t sec CPU, %w sec real\n");
@@ -287,9 +350,20 @@ int main(int argc, char *argv[])
               % d2u(sH - s3) % (sH - s3));
     }
 
-    if (vm["algo"].as<std::string>() == "sumT" ||
-        vm["algo"].as<std::string>() == "sum3HT" ||
-        vm["algo"].as<std::string>() == "all") {
+    if (sum & SUM_Q) {
+        v = input_vector;
+        TRACE(("\nCalculating sumQ...\n"));
+        boost::timer::auto_cpu_timer t("Done in %t sec CPU, %w sec real\n");
+        sQ = sumQ(v);
+        TRACE(("sumQ = 0x%016x = % .40e\n"
+               "dif0 = 0x%016x = % .40e\n"
+               "dif3 = 0x%016x = % .40e\n")
+              % d2u(sQ) % sQ
+              % d2u(sQ - s0) % (sQ - s0)
+              % d2u(sQ - s3) % (sQ - s3));
+    }
+
+    if (sum & SUM_T) {
         v = input_vector;
         TRACE(("\nCalculating sum table...\n"));
         boost::timer::auto_cpu_timer t("Done in %t sec CPU, %w sec real\n");
